60d96c9366bf04a73c422e7913a34eaf277aeaaa
satwikWiz <satwik@wizcommerce.com>
2025-10-22 11:16:27 +0530

    feat : workflows initiated

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { requireAuth } from '@/lib/auth-utils'
-
+import { WorkflowsList, WorkflowsContainer } from '@/features/workflows/components/workflows';
+import { prefetchWorkflows } from '@/features/workflows/server/prefetch';
+import { HydrateClient } from '@/TRPC/Server';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Page = async () => {
   await requireAuth();
+  prefetchWorkflows();
   return (
-    <div>Workflows</div>
+    <div>
+      <WorkflowsContainer>
+        <HydrateClient>
+          <ErrorBoundary fallback={<div>Error</div>}>
+            <Suspense fallback={<div>Loading...</div>}></Suspense>
+            <WorkflowsList />
+          </ErrorBoundary>
+        </HydrateClient>
+      </WorkflowsContainer>
+    </div>
   )
 }
 
